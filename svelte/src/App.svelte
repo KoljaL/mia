dacb503a4892bb894bedd9fd3e8d78fd490cc545
Kolja <lasar@rasal.de>
2022-08-11 09:59:41 +0200

    remove daisyui & tailwind files

--- svelte/src/App.svelte
+++ svelte/src/App.svelte
@@ -11,22 +11,3 @@
 <Navbar bind:sidebar={open} />
 
 <Main />
-
-<style>
-    :global(body) {
-        /* color: var(--text-prim);
-        background: var(--bg-prim);
-        transition: background-color 0.3s;
-        transition: color 0.3s; */
-        padding: 0;
-    }
-
-    /* Hide scrollbar for Chrome, Safari and Opera, IE, Edge and Firefox */
-    :global(*)::-webkit-scrollbar {
-        display: none;
-    }
-    :global(*) {
-        -ms-overflow-style: none;
-        scrollbar-width: none;
-    }
-</style>
